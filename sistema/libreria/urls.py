6f8cb3244f4faa833bf89d8399d9f492fef137a4
JordinRosario <jordinjose2003@gmail.com>
2022-11-27 15:04:52 -0500

    Agregar-Register/Login

--- sistema/libreria/urls.py
+++ sistema/libreria/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     #""" Interfaz de los libros """
     path('libros', views.libros, name='libros'),
     path('libros/crear', views.crear_libro, name='crearlibros'),
-    path('libros/editar', views.editar, name='editar')
+    path('libros/editar', views.editar, name='editar'),
+    path('crear/sesion', views.crear_sesion, name = 'crearsesion'),
+    path('cerrar/sesion', views.cerrar_sesion, name = 'cerrarsesion'),
+    path('iniciar/sesion', views.iniciar_sesion, name = 'iniciarsesion'),    
 ]
--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,10 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
+def base(request):
+    return render({'user':User})
+
 def inicio(request):
-    return render(request, 'paginas/inicio.html')
+    return render(request, 'paginas/inicio.html' )
+
+def crear_sesion(request):
+    if request.method == 'GET':
+        return render(request, 'registrate/crear_sesion.html',{
+            'crear_sesion':UserCreationForm
+        })
+    else:
+        print(request.POST)
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'] )
+                user.save()
+                login(request,user)
+                return redirect('inicio')
+            except:
+                return render(request, 'registrate/crear_sesion.html',{
+                'crear_sesion':UserCreationForm,
+                'error':'El usuario ya esta creado'
+        })
+        return render(request, 'registrate/crear_sesion.html',{
+            'crear_sesion':UserCreationForm,
+            'error_clave':'Las claves no coinsiden'
+        })
+    
+def iniciar_sesion(request):
+    if request.method == 'GET':
+        return render(request, 'registrate/iniciar_sesion.html',{
+            'iniciar_sesion_form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user is None:
+            return render(request, 'registrate/iniciar_sesion.html',{
+            'iniciar_sesion_form': AuthenticationForm,
+            'error':'La clave es incorrecta o cuenta no encontrada'
+        })
+        else:
+            login(request, user)
+            return redirect('inicio')
+    
+    
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('inicio')
+
+
+    
+    
+    
+    
 def nosotros(request):
     return render(request,'paginas/nosotros.html') 
 
